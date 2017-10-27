92d1a4fee7330a98da8ff4d4f7d11a77d72e7e7d
Your Name <you@example.com>
2017-10-27 20:08:07 +0200

    Adding navbar with routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
+
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  constructor( public viewContainerRef : ViewContainerRef){}
+  name = 'Angular'; }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,37 @@
 import { NgModule    }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent }  from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { SearchComponent } from './components/search/search.component';
+
+const appRoutes: Routes = [
+  { path: '', component: SearchComponent },
+  { path: 'about',component: AboutComponent , data:'About' },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
-  imports:      [    BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [BrowserModule ,    
+  RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ) ],
+  declarations: [ AppComponent, NavbarComponent, AboutComponent, SearchComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
